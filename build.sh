9d3b5c642b3b4f7b3c9d4a87b58463c31c682bed
seiko <seiko_des@outlook.com>
2024-12-24 09:27:57 +0800

    build in ubuntu 24.04

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 # 更新并安装依赖 https://openwrt.org/docs/guide-developer/toolchain/install-buildsystem#debianubuntumint
-# sudo apt update -y
-# sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint \
-#     binutils bison build-essential bzip2 ccache clang cmake cpio curl \
-#     device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib \
-#     g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 \
-#     libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev \
-#     libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool \
-#     libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full \
-#     patch pkgconf python3 python3-pip python3-ply python3-docutils python3-pyelftools \
-#     qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl \
-#     unzip vim wget xmlto xxd zlib1g-dev g++ libncurses5-dev file
-# sudo apt clean
+sudo apt update -y
+sudo apt install build-essential clang flex bison g++ gawk \
+    gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
+    python3-setuptools rsync swig unzip zlib1g-dev file wget
+sudo apt clean
 
 ROOT_DIR=$PWD
 LOG_FILE=$ROOT_DIR/build.log
@@ -43,4 +36,4 @@ make defconfig -j$(nproc)
 
 # 下载和编译
 make download -j$(nproc) 2>&1 | tee "$LOG_FILE"
-make -j1 V=s 2>&1 | tee "$LOG_FILE"
+make -j$(nproc) V=s 2>&1 | tee "$LOG_FILE"
